81c2faf01501ee7dee748a885dd0de6e45775914
unknown <agent0308@gmail.com>
2020-05-17 13:04:04 +0300

    Создал папку jmx и начал работать над MBeanом

--- src/main/java/com/despair/Application.java
+++ src/main/java/com/despair/Application.java
@@ -1,12 +1,20 @@
 package com.despair;
 
+import com.despair.jmx.DatabaseManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedObjectNameException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("jmx:type=com.despair.jmx.DatabaseManager");
+        DatabaseManager databaseManager = new DatabaseManager();
+        server.registerMBean(databaseManager, name);
         SpringApplication.run(Application.class, args);
     }
 
